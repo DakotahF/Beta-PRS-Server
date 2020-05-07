87fba4f812251d3591c97bd9094a863c413c7c58
Dakotah Feil <kotah@wonderland.sph.umich.edu>
2020-05-07 15:34:38 -0400

    removes extraneous content

--- tests/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /net/hunt/home/kotah/prs-server-beta/PRS-methods/prs-toolchain/tests
-# Build directory: /net/hunt/home/kotah/prs-server-beta/PRS-methods/prs-toolchain/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(PRS_PRS "/usr/bin/cmake" "-DPRSCALC=/net/hunt/home/kotah/prs-server-beta/PRS-methods/prs-toolchain/tests/src/prs-calc" "-DTESTPRS=/net/hunt/home/kotah/prs-server-beta/PRS-methods/prs-toolchain/tests/testprs" "-P" "/net/hunt/home/kotah/prs-server-beta/PRS-methods/prs-toolchain/tests/test_01/prs_compare.cmake")
